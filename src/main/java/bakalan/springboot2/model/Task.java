4edede3daf000115f0ccd792c6486e3e892df4e0
baka-lan <evg.bakl@gmail.com>
2020-04-06 03:58:01 +0700

    created data classes

---
+++ src/main/java/bakalan/springboot2/model/Task.java
@@ -0,0 +1,76 @@
+package bakalan.springboot2.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "order_id")
+    long orderId;
+
+    @Column(name = "date")
+    LocalDate date;
+
+    @Column(name = "time")
+    LocalTime time;
+
+    @Column(name = "resolved")
+    boolean resolved;
+
+    public Task() {
+    }
+
+    public Task(long orderId, LocalDate date, LocalTime time, boolean resolved) {
+        this.orderId = orderId;
+        this.date = date;
+        this.time = time;
+        this.resolved = resolved;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        this.resolved = resolved;
+    }
+}
